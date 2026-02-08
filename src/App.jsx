7787703f3bea47e246feecc36cc9c04f12e0b855
Anit <110488522+kand5950@users.noreply.github.com>
2026-02-08 17:22:54 -0500

    component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 
 import './App.css'
 
+//COMPONENT = Function that returns JSX
+function Greeting(){
+  return <h1>Hello, I am Pedro!</h1>
+}
+
 function App() {
 
-  const name = "Pedro";
-  const age = 24;
+  // const name = getName();
+  // const age = 24;
 
   return <div>
-    <h1>
-      Hello World, I am {name}, I am {age} years old
-    </h1>
+    <Greeting />
+    <Greeting />
+    <Greeting />
+    <Greeting />
     </div>;
 }
+
+// function getName() {
+//   return "Jake"
+// }
+
+
 
 export default App
